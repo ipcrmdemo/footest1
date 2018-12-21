883d386c383153b1540042bfd157990543aabad7
Matt Cadorette <mcadorette@gmail.com>
2018-12-20 20:43:13 -0500

    Initial commit from Atomist

---
+++ src/main/java/com/footest1/Footest1Application.java
@@ -0,0 +1,12 @@
+package com.footest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Footest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Footest1Application.class, args);
+	}
+}
